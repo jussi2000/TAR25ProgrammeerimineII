c84225e47dd2de6eaf4998430e1a9929f78947b6
Opilane Techno+TLN <johannat2009@gmail.com>
2026-09-03 13:42:20 +0300

    DbContext

---
+++ OnlineStoreTAR25/OnlineStoreTAR25/Controllers/SpaceshipController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopTAR25.Controllers
+{
+    public class SpaceshipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //teha Data projecti alla ShopTAR25Context nimega class
+    }
+}
